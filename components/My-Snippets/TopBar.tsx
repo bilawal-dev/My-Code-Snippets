8b16012106b8dd85fce6f54419910476b8593edc
bilawal-dev <bk4449719@gmail.com>
2026-06-03 09:00:00 +0500

    Added UX Hover Effect On Logut Button

--- components/My-Snippets/TopBar.tsx
+++ components/My-Snippets/TopBar.tsx
@@ -87,7 +87,7 @@ export default function TopBar({ session, status }: { session: Session | null, s
                                     <div className='text-base'>{session?.user?.name}</div>
                                     <div className='text-xs text-slate-500'>{session?.user?.email}</div>
                                 </li>
-                                <li onClick={() => signOut()} className='block cursor-pointer text-base px-4 py-2 text-gray-800 hover:bg-gray-100'>
+                                <li onClick={() => signOut()} className='block text-base px-4 py-2 text-gray-800 hover:bg-gray-100'>
                                     Logout
                                 </li>
                             </ul>
