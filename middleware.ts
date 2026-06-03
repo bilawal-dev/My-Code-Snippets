2fdf4e5a22b634c4f596604f539944e0a1e237ff
bilawal-dev <bk4449719@gmail.com>
2026-06-03 18:00:00 +0500

    Fixed Signining Bug

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,3 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+export { default } from "next-auth/middleware"
 
-export async function middleware(req: any) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const { pathname } = req.nextUrl;
-
-  if (pathname.startsWith("/my-snippets")) {
-    if (!token) {
-      return NextResponse.redirect(new URL("/sign-in", req.url));
-    }
-  }
-
-  return NextResponse.next();
-}
+export const config = { matcher: ["/my-snippets(.*)"] }
